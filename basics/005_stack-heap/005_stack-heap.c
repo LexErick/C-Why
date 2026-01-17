7c75f70b99ec06b2f8aea48ebd40d6b068f99d51
LexErick <lexerick.c@gmail.com>
2026-01-17 16:04:02 +0000

    Update: Lesson 5 - Heap & Stack

---
+++ basics/005_stack-heap/005_stack-heap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//void f(void) {
+//  int x = 42;
+//  printf("%d\n", x);
+//}
+
+//int main(void) {
+//  f();
+//  return 0;
+//}
+
+/* If Below Function Stand Alonw It Will Not Work */
+int *good(void) {
+
+    int *p = malloc(sizeof(int));
+    *p  = 84;
+
+    return p;
+}
+
+int main (void) {
+
+    int x = good();
+
+    printf("Value: %d\n", x);
+
+    free(x);
+    x = NULL;
+
+    return 0;
+}
